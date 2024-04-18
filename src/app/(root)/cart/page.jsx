d2f4602ba48e67576405d57076b936a66895695b
Bijan <bijan2059@gmail.com>
2024-04-18 20:27:14 +0545

    removed the cartlist component

--- src/app/(root)/cart/page.jsx
+++ src/app/(root)/cart/page.jsx
@@ -1,30 +1,25 @@
-import CartList from "@/components/CartComponent/CartList";
 import CartComponent from "@/components/CartComponent/CartComponent";
 import CheckOutComponent from "@/components/CartComponent/CheckOutComponent";
 
 function Index() {
   return (
     <>
-    <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
-    <h1 className='text-2xl px-5 md:px-0 mt-2 font-bold md:text-3xl mb-2'>Your Cart</h1>
-    </div>
-     
+      <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
+        <h1 className='text-xl px-5 md:px-0 mt-2 font-bold md:text-2xl mb-2'>Your Cart</h1>
+      </div>
       <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
         {/* Cart Part  */}
         <div className="w-full px-5 flex flex-col gap-3 my-5 md:px-0 ">
-        
           <CartComponent />
           <CartComponent />
         </div>
+        {/* Cart Part  */}
         {/* Checkout Part  */}
-        <div className="w-full px-5 my-5 md:my-5 md:px-0 md  md:w-2/5">
-          <CheckOutComponent/>
+        <div className="w-full px-5 my-5 md:my-5 md:px-0  md:w-1/2">
+          <CheckOutComponent />
         </div>
         {/* Checkout Part  */}
-        </div>
-      {/* gap  */}
-      {/* <div className="h-52" /> */}
-      {/* gap  */}
+      </div>
     </>
   );
 }
