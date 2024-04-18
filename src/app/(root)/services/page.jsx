a623f127254cba800a8d016cd2145285b66a59a1
Bijan <bijan2059@gmail.com>
2024-04-18 20:27:34 +0545

    added services in service page

--- src/app/(root)/services/page.jsx
+++ src/app/(root)/services/page.jsx
@@ -1,9 +1,47 @@
+import ServiceComponent from "@/components/Design/ServiceComponent";
+
 const Services = () => {
+  const services = [
+    {
+      service: "Expert Consultation",
+      description: "Receive personalized advice from guitar specialists to choose the ideal instrument based on skill level, musical style, and budget.",
+      icon: "GrUserExpert"
+    },
+    {
+      service: "Customization Options",
+      description: "Customize guitars to reflect individuality by modifying finish, hardware, and electronics.",
+      icon: "GiGuitarBassHead"
+    },
+    {
+      service: "Professional Setup",
+      description: "Ensure optimal playability, tone, and intonation with precision instrument setup performed by skilled technicians.",
+      icon: "MdOutlineDashboardCustomize"
+    },
+    {
+      service: "Convenient Financing",
+      description: "Spread the cost of purchases with flexible financing options, making high-quality guitars accessible to all budgets.",
+      icon: "FaMoneyCheck"
+    },
+    {
+      service: "Comprehensive Warranty",
+      description: "Protect investments with comprehensive warranty coverage against defects in materials and workmanship.",
+      icon: "MdOutlineSecurity"
+    },
+    {
+      service: "Educational Resources",
+      description: "Access tutorials, tips, and guides to enhance playing skills, instrument maintenance, and overall musical journey.",
+      icon: "FaGoogleScholar"
+    }
+  ];
+
   return (
     <div className=" md:w-3/4 gap-2 md:mx-auto">
-      <h1 className='text-2xl px-5 md:px-0 mt-2 font-bold md:text-3xl mb-2'>Services</h1>
-      <div class="w-full grid p-3 grid-cols-2 md:grid-cols-3 gap-4">
-        <div class="bg-gray-200 p-4">Item 1</div>
+      <h1 className='text-2xl px-5 md:px-0 mt-2 font-bold md:text-3xl mb-4 md:mb-6'>Services</h1>
+      <div className="w-full grid px-8 gap-4 grid-cols-1 md:grid-cols-2 md:px-5 lg:grid-cols-3 ">
+        {services.map((service, index) => (
+          <ServiceComponent key={index} service={service} />
+        ))}
+
       </div>
     </div>
   )
