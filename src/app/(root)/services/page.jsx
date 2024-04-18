6424f340501d7def510588b22f6132c55a6fbf37
Bijan <bijan2059@gmail.com>
2024-04-18 18:35:32 +0545

    added services page

---
+++ src/app/(root)/services/page.jsx
@@ -0,0 +1,12 @@
+const Services = () => {
+  return (
+    <div className=" md:w-3/4 gap-2 md:mx-auto">
+      <h1 className='text-2xl px-5 md:px-0 mt-2 font-bold md:text-3xl mb-2'>Services</h1>
+      <div class="w-full grid p-3 grid-cols-2 md:grid-cols-3 gap-4">
+        <div class="bg-gray-200 p-4">Item 1</div>
+      </div>
+    </div>
+  )
+}
+
+export default Services
