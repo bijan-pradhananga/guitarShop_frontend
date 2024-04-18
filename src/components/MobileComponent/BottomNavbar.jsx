f7a77abba3fbfac56af8bd430fccc8b1d4db6ef7
Bijan <bijan2059@gmail.com>
2024-04-18 18:35:58 +0545

    added services section in navbar

--- src/components/MobileComponent/BottomNavbar.jsx
+++ src/components/MobileComponent/BottomNavbar.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { CiUser,CiShoppingCart,CiHeart,CiHome } from "react-icons/ci";
 import { RxHamburgerMenu } from "react-icons/rx";
 
@@ -5,8 +6,8 @@ const BottomNavbar = ({setIsSidebar}) => {
   return (
     <div className='w-full h-16 px-8 fixed bottom-0 flex justify-between items-center bg-white dark:bg-inherit dark:text-gray-200 border-t z-10 border-gray-300 md:justify-center md:gap-10 lg:hidden '>
         <RxHamburgerMenu onClick={()=>{setIsSidebar(true)}} className="text-2xl cursor-pointer"/>
-        <CiShoppingCart className="text-3xl cursor-pointer"/>
-        <CiHome className="text-3xl cursor-pointer"/>
+        <Link href='/cart'><CiShoppingCart className="text-3xl cursor-pointer"/></Link>
+        <Link href='/'><CiHome className="text-3xl cursor-pointer"/></Link>
         <CiHeart className="text-3xl cursor-pointer"/>
         <CiUser className="text-3xl cursor-pointer"/>
 
--- src/components/RootComponent/NavbarComponent2.jsx
+++ src/components/RootComponent/NavbarComponent2.jsx
@@ -12,7 +12,7 @@ const NavbarComponent2 = () => {
           <Link href='/about'>About</Link>
         </li>
         <li className='h-full flex items-center border-b-2 border-transparent transition-colors duration-300 hover:border-blue-600 cursor-pointer'>
-          <Link href='/category'>Contact</Link>
+          <Link href='/category'>Services</Link>
         </li>
         <li className='h-full flex items-center border-b-2 border-transparent transition-colors duration-300 hover:border-blue-600 cursor-pointer'>
           <Link href='/category'>Category</Link>
