47f7ef31aa81e0a598ca0363fba5edb31c55c832
Bijan <bijan2059@gmail.com>
2024-04-19 07:26:36 +0545

    made a wishlist page

---
+++ src/app/(root)/wishlist/page.jsx
@@ -0,0 +1,19 @@
+import ProductCard from "@/components/ProductComponent/ProductCard"
+
+const Wishlist = () => {
+    return (
+        <>
+            <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
+                <h1 className='text-xl px-5 md:px-0 mt-2 font-bold md:text-2xl mb-2'>WishList</h1>
+            </div>
+            <div className="w-full md-2 md:mb-10 mt-4 px-5 grid grid-cols-2 gap-2 md:px-0 md:w-3/4 md:mx-auto md:grid-cols-3 lg:grid-cols-4 md:gap-4 ">
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+                <ProductCard />
+            </div>
+        </>
+    )
+}
+
+export default Wishlist
