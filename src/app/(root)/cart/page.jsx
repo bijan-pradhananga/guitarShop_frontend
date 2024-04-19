70c03aca78a57061d7f835baaae9ab2ee54c608c
Bijan <bijan2059@gmail.com>
2024-04-19 07:26:10 +0545

    added padding to the main container

--- src/app/(root)/cart/page.jsx
+++ src/app/(root)/cart/page.jsx
@@ -7,15 +7,15 @@ function Index() {
       <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
         <h1 className='text-xl px-5 md:px-0 mt-2 font-bold md:text-2xl mb-2'>Your Cart</h1>
       </div>
-      <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
+      <div className="md:flex px-5 md:w-3/4 gap-2 md:px-0 md:mx-auto">
         {/* Cart Part  */}
-        <div className="w-full px-5 flex flex-col gap-3 my-5 md:px-0 ">
+        <div className="w-full flex flex-col gap-3 my-5 md:px-0 ">
           <CartComponent />
           <CartComponent />
         </div>
         {/* Cart Part  */}
         {/* Checkout Part  */}
-        <div className="w-full px-5 my-5 md:my-5 md:px-0  md:w-1/2">
+        <div className="w-full  my-5 md:my-5  md:w-1/2">
           <CheckOutComponent />
         </div>
         {/* Checkout Part  */}
