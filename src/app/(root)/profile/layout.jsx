021c0d8354f2caa90ea2470c37ee9c27cac40df8
Bijan <bijan2059@gmail.com>
2024-04-19 20:25:21 +0545

    removed the profile navbar

--- src/app/(root)/profile/layout.jsx
+++ src/app/(root)/profile/layout.jsx
@@ -5,14 +5,6 @@ export default function ProfileLayout({
   }) {
     return (
       <div className="w-full px-5 flex flex-col gap-2 my-6 md:my-6 md:px-0 md:w-3/4  md:mx-auto">
-        <h1 className='text-xl font-bold md:text-3xl'>My Account</h1>
-        <div className="border-b border-gray-400">
-        <ul className="w-full flex justify-center gap-6 p-3 text-gray-600 dark:text-gray-400 md:text-lg md:gap-10 font-semibold uppercase">
-          <li><Link href='/profile' >Profile</Link></li>
-          <li><Link href='/profile/orders' >My Orders</Link></li>
-          <li><Link href='/services' >Edit Profile</Link></li>
-        </ul>
-        </div>
         {children}
       </div>
     )
