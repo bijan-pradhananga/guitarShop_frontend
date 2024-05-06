c64dcb20a9a5c97b43d6df09c2792b0703d52c82
Bijan <bijan2059@gmail.com>
2024-05-06 14:16:19 +0545

    used redux store to get the items

--- src/app/(root)/page.jsx
+++ src/app/(root)/page.jsx
@@ -1,30 +1,37 @@
+'use client'
 import ProductCard from '@/components/Design/ProductComponent/ProductCard'
 import ProductListSlider from '@/components/Design/ProductComponent/ProductListSlider'
 import BannerComponent from '@/components/RootComponent/BannerComponent'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks';
+import { fetchProducts } from '@/lib/features/product';
+import { useEffect } from 'react';
 
 const page = () => {
-
+  const dispatch = useAppDispatch()
+  const products = useAppSelector((state) => state.product)
+  useEffect(() => {
+    dispatch(fetchProducts('product'))
+}, [])
   return (
     <>
-    <BannerComponent/>
-    {/* Trending Component  */}
+      <BannerComponent />
+      {/* Trending Component  */}
       <div className="w-full my-4 md:my-6 px-5  md:px-0 md:w-3/4 md:mx-auto">
         <h1 className='font-bold text-xl text-gray-600 dark:text-gray-300 md:text-2xl'>Trending</h1>
       </div>
       <div className="w-full md-2 md:mb-10 mt-1 px-5 grid grid-cols-2 gap-2 md:px-0 md:w-3/4 md:mx-auto lg:grid-cols-4 md:gap-4 ">
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
+        {products.data.map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
       </div>
-     {/* Trending Component  */}
-     {/* Deal of the day  */}
+      {/* Trending Component  */}
+      {/* Deal of the day  */}
       <div className="w-full mt-4 md:my-6 px-5 md:px-0 md:w-3/4 md:mx-auto">
-          <h1 className='font-bold text-xl text-gray-600 dark:text-gray-300 md:text-2xl'>Deal of the Day</h1>
+        <h1 className='font-bold text-xl text-gray-600 dark:text-gray-300 md:text-2xl'>Deal of the Day</h1>
       </div>
       <div className="w-full mt-3 px-5 md:px-3  md:w-3/4 md:mx-auto">
-      <ProductListSlider/>
-    
+        <ProductListSlider />
+
       </div>
       {/* Deal of the day  */}
     </>
