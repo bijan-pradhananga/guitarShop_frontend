2d92a9171f5c07719eb40a00e211c834f4a0248f
Bijan <bijan2059@gmail.com>
2024-05-08 14:40:15 +0545

    slight change to wishlist btn

--- src/components/hero/AddToWishlistBtn.jsx
+++ src/components/hero/AddToWishlistBtn.jsx
@@ -1,10 +1,11 @@
-import { FaRegHeart } from "react-icons/fa";
+
 
 const AddToWishlistBtn = () => {
     return (
         <button className="w-full flex justify-center items-center gap-1 bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded">
-           <FaRegHeart className="text-lg"/>  Add to WishList
+            Add to Wishlist
         </button>
+
     )
 }
 
