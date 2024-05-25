365d680f8ce14fdf1a663e74090b38fc45123218
Bijan <bijan2059@gmail.com>
2024-05-25 13:27:52 +0545

    updated the dark mode to not show the style error

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,8 +1,23 @@
 'use client'
-// dark mode provider 
-import { ThemeProvider } from "next-themes"
+//dark mode provider
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from 'next-themes';
 
-export function Providers({children}){
-    return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}</ThemeProvider>
+export function Providers({ children }) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        // Return a loading spinner or a fallback UI if necessary
+        return <>{children}</>; 
+    }
+
+    return (
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+        </ThemeProvider>
+    );
 }
