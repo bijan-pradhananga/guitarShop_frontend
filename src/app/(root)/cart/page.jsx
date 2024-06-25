d56826f7c895a30ec7eef1b2f794112d4161e5d6
Bijan <bijan2059@gmail.com>
2024-06-25 15:03:40 +0545

    made cart page client component

--- src/app/(root)/cart/page.jsx
+++ src/app/(root)/cart/page.jsx
@@ -1,7 +1,20 @@
+'use client'
+import withAuth from "@/app/authProvider"
 import CartComponent from "@/components/Design/CartComponent/CartComponent";
 import CheckOutComponent from "@/components/Design/CartComponent/CheckOutComponent";
+import cart, { fetchCartItems } from "@/lib/features/cart";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { useEffect } from "react";
 
-function Index() {
+function CartPage() {
+  const dispatch = useAppDispatch()
+  const user = useAppSelector((state) => state.user.data);
+  const {items,isLoading} = useAppSelector((state) => state.cart);
+  useEffect(() => {
+    if (user && user._id) {
+      dispatch(fetchCartItems(user._id));
+    }
+  }, [dispatch, user]);
   return (
     <>
       <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
@@ -10,8 +23,15 @@ function Index() {
       <div className="md:flex px-5 md:w-3/4 gap-2 md:px-0 md:mx-auto">
         {/* Cart Part  */}
         <div className="w-full flex flex-col gap-3 my-5 md:px-0 ">
-          <CartComponent />
-          <CartComponent />
+        {isLoading ? (
+            <div>Loading...</div>
+          ) : items.length > 0 ? (
+            items.map((item) => (
+              <CartComponent key={item._id} item={item} />
+            ))
+          ) : (
+            <div>Your cart is empty</div>
+          )}
         </div>
         {/* Cart Part  */}
         {/* Checkout Part  */}
@@ -24,4 +44,4 @@ function Index() {
   );
 }
 
-export default Index;
+export default withAuth(CartPage);
