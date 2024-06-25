cd7a60081d04ea58dab27230893bb1f9c81b0fb7
Bijan <bijan2059@gmail.com>
2024-06-25 15:04:37 +0545

    applied utils

--- src/components/Design/CartComponent/CartComponent.jsx
+++ src/components/Design/CartComponent/CartComponent.jsx
@@ -1,8 +1,10 @@
-const CartComponent = () => {
+import { capitalizeFirstLetter } from "@/utils/utils"
+
+const CartComponent = ({item}) => {
   return (
     <div className="w-full flex gap-4 p-2 rounded dark:border-gray-700 dark:border-2  shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-      <CartImgPart />
-      <CartInfoPart />
+      <CartImgPart item={item}/>
+      <CartInfoPart item={item}/>
     </div>
   )
 }
@@ -15,19 +17,19 @@ const CartImgPart = () => {
   )
 }
 
-const CartInfoPart = () => {
+const CartInfoPart = ({item}) => {
   return (
     <div className="w-4/6 p-1 flex flex-col justify-between text-sm lg:text-lg lg:p-3">
       <div>
         <div className="w-full flex justify-between my-1 ">
-          <h1 className="text-md  lg:text-xl">Aria Guitar</h1>
-          <h2>Rs. 20000</h2>
+          <h1 className="text-md  lg:text-xl">{item.product_id.product_name}</h1>
+          <h2>{item.product_id.price}</h2>
         </div>
-        <h3>Acoustic</h3>
+        <h3> {capitalizeFirstLetter(item.product_id.category_id.category_name)}</h3>
       </div>
   
       <div className="w-full flex justify-between items-center ">
-        <h1 >Qty: 1</h1>
+        <h1 >Qty: {item.quantity}</h1>
         <h2 className="text-red-500 ">Remove</h2>
       </div>
     </div>
