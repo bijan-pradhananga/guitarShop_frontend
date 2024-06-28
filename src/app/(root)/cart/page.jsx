767e00aafb5ea37c699ed2fc47b40e06aa8e56ae
Bijan <bijan2059@gmail.com>
2024-06-28 17:10:22 +0545

    added remove from cart function

--- src/app/(root)/cart/page.jsx
+++ src/app/(root)/cart/page.jsx
@@ -2,7 +2,7 @@
 import withAuth from "@/app/authProvider"
 import CartComponent from "@/components/Design/CartComponent/CartComponent";
 import CheckOutComponent from "@/components/Design/CartComponent/CheckOutComponent";
-import cart, { fetchCartItems } from "@/lib/features/cart";
+import cart, { fetchCartItems, removeFromCart } from "@/lib/features/cart";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useEffect } from "react";
 
@@ -15,6 +15,20 @@ function CartPage() {
       dispatch(fetchCartItems(user._id));
     }
   }, [dispatch, user]);
+
+  const handleRemoveFromCart = async (product_id) =>{
+    const cartData = { product_id, user_id: user._id };
+    const result = await dispatch(removeFromCart(cartData));
+    if (result.meta.requestStatus === 'fulfilled') {
+        alert('Product removed from cart successfully');
+    } else {
+        alert('Failed to remove product from cart');
+    }
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
       <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
@@ -23,11 +37,9 @@ function CartPage() {
       <div className="md:flex px-5 md:w-3/4 gap-2 md:px-0 md:mx-auto">
         {/* Cart Part  */}
         <div className="w-full flex flex-col gap-3 my-5 md:px-0 ">
-        {isLoading ? (
-            <div>Loading...</div>
-          ) : items.length > 0 ? (
-            items.map((item) => (
-              <CartComponent key={item._id} item={item} />
+        { items.length > 0  ? (
+            items.map((item,index) => (
+              <CartComponent key={index} item={item} handleRemoveFromCart={handleRemoveFromCart}/>
             ))
           ) : (
             <div>Your cart is empty</div>
