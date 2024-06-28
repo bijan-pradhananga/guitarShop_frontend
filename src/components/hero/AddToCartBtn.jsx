f38bb74898f8e5c1eaf5b007d64b0c86065cd763
Bijan <bijan2059@gmail.com>
2024-06-28 17:13:50 +0545

    added decrement quantity

--- src/components/hero/AddToCartBtn.jsx
+++ src/components/hero/AddToCartBtn.jsx
@@ -1,15 +1,17 @@
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { addToCart } from "@/lib/features/cart";
 import { checkAuth } from "@/lib/features/user";
+import { decrementQuantity } from "@/lib/features/product";
 
-const AddToCartBtn = ({product}) => {
+const AddToCartBtn = ({product,quantity}) => {
     const dispatch = useAppDispatch();
-
+    // console.log(product);
       const handleAddToCart = async () => {
         const res = await dispatch(checkAuth())
         if (res.payload.success) {
             const user_id = res.payload.user._id 
-            dispatch(addToCart({ product_id: product._id, user_id:user_id }));
+            await dispatch(addToCart({ product_id: product._id, user_id:user_id, quantity }));
+            dispatch(decrementQuantity({quantity}))
             alert('Product added to cart successfully');
         }
         else {
