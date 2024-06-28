8bbc0ec195a63ea365244ccb604e2c33a64c041d
Bijan <bijan2059@gmail.com>
2024-06-28 17:14:01 +0545

    made out of stock component

---
+++ src/components/hero/OutOfStockComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OutOfStockComponent = () => {
+  return (
+    <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded"
+    >
+        Out Of Stock
+    </button>
+  )
+}
+
+export default OutOfStockComponent
