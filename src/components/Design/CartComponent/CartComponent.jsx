ff8ba73611008a1cb120ffcaeb0de1bf6d657147
Bijan <bijan2059@gmail.com>
2024-06-28 17:13:26 +0545

    made cart component dynamic

--- src/components/Design/CartComponent/CartComponent.jsx
+++ src/components/Design/CartComponent/CartComponent.jsx
@@ -1,23 +1,26 @@
 import { capitalizeFirstLetter } from "@/utils/utils"
 
-const CartComponent = ({item}) => {
+const CartComponent = ({item,handleRemoveFromCart}) => {
+  if (!item.product_id) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="w-full flex gap-4 p-2 rounded dark:border-gray-700 dark:border-2  shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <CartImgPart item={item}/>
-      <CartInfoPart item={item}/>
+      <CartInfoPart item={item} handleRemoveFromCart={handleRemoveFromCart}/>
     </div>
   )
 }
 
-const CartImgPart = () => {
+const CartImgPart = ({item}) => {
   return (
     <div className="w-1/3 aspect-square p-1 ">
-      <img src="/guitar.png" className="w-full h-full object-cover rounded" />
+      <img src={`http://localhost:3001/products/${item.product_id.product_image}`} className="w-full h-full object-cover rounded" />
     </div>
   )
 }
 
-const CartInfoPart = ({item}) => {
+const CartInfoPart = ({item,handleRemoveFromCart}) => {
   return (
     <div className="w-4/6 p-1 flex flex-col justify-between text-sm lg:text-lg lg:p-3">
       <div>
@@ -30,7 +33,7 @@ const CartInfoPart = ({item}) => {
   
       <div className="w-full flex justify-between items-center ">
         <h1 >Qty: {item.quantity}</h1>
-        <h2 className="text-red-500 ">Remove</h2>
+        <h2 className="text-red-500 cursor-pointer" onClick={()=>{handleRemoveFromCart(item.product_id._id)}}>Remove</h2>
       </div>
     </div>
   )
