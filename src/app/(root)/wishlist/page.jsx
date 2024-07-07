4b5c291f96a24c367507b14afd3bc5b5b9b5318e
Bijan <bijan2059@gmail.com>
2024-07-07 12:31:33 +0545

    made wishlist page

--- src/app/(root)/wishlist/page.jsx
+++ src/app/(root)/wishlist/page.jsx
@@ -1,19 +1,49 @@
-import ProductCard from "@/components/Design/ProductComponent/ProductCard"
+'use client'
+import withAuth from "@/app/authProvider"
+import WishListCard from "@/components/Design/WishListComponent/WishListCard"
+import ProductCardLoader from "@/components/Loader/ProductCardLoader/ProductCardLoader"
+import { fetchWishlistItems, removeFromWishlist } from "@/lib/features/wishlist"
+import { useAppDispatch, useAppSelector } from "@/lib/hooks"
+import { useEffect } from "react"
 
 const Wishlist = () => {
-    return (
-        <>
-            <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
-                <h1 className='text-xl px-5 md:px-0 mt-2 font-bold md:text-2xl mb-2'>WishList</h1>
-            </div>
-            <div className="w-full md-2 md:mb-10 mt-4 px-5 grid grid-cols-2 gap-2 md:px-0 md:w-3/4 md:mx-auto md:grid-cols-3 lg:grid-cols-4 md:gap-4 ">
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-                <ProductCard />
-            </div>
-        </>
-    )
+  const dispatch = useAppDispatch()
+  const user = useAppSelector((state) => state.user.data);
+  const {items,isLoading} = useAppSelector((state) => state.wishlist);
+  useEffect(() => {
+    if (user && user._id) {
+      dispatch(fetchWishlistItems(user._id));
+    }
+  }, [dispatch, user]);
+
+  const handleRemoveFromWishlist = async (product_id) =>{
+    const cartData = { product_id, user_id: user._id };
+    const result = await dispatch(removeFromWishlist(cartData));
+    if (result.meta.requestStatus === 'fulfilled') {
+        alert('Product removed from wishlist successfully');
+    } else {
+        alert('Failed to remove product from cart');
+    }
+  }
+
+  return (
+    <>
+    <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
+        <h1 className='text-xl px-5 md:px-0 mt-4 font-bold md:text-2xl mb-2'>WishList</h1>
+    </div>
+    <div className="w-full md-2 md:mb-10 mt-4 px-5 grid grid-cols-2 gap-2 md:px-0 md:w-3/4 md:mx-auto lg:grid-cols-4 md:gap-4 ">
+        {isLoading ? (
+          // Show loading indicator
+          <ProductCardLoader count={4} />
+        ) : (
+          // Render products
+          items.map((item, index) => (
+            <WishListCard key={item.product_id._id} product={item.product_id} handleRemoveFromWishlist={handleRemoveFromWishlist} />
+          ))
+        )}
+      </div>
+</>
+  )
 }
 
-export default Wishlist
+export default withAuth(Wishlist) 
