b5590223c50019ee4189dbb3ebff321ba9824106
Bijan <bijan2059@gmail.com>
2024-07-07 12:32:27 +0545

    made an addtowishlist button

--- src/components/hero/AddToWishlistBtn.jsx
+++ src/components/hero/AddToWishlistBtn.jsx
@@ -1,8 +1,28 @@
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { checkAuth } from "@/lib/features/user";
+import { addToWishlist } from "@/lib/features/wishlist";
 
 
-const AddToWishlistBtn = () => {
+const AddToWishlistBtn = ({product}) => {
+    const dispatch = useAppDispatch();
+    const {error} = useAppSelector((state) => state.wishlist);
+    const handleAddToWishlist = async () => {
+        const res = await dispatch(checkAuth())
+        if (res.payload.success) {
+            const user_id = res.payload.user._id 
+            const response = await dispatch(addToWishlist({ product_id: product._id, user_id:user_id }));
+            if (response.payload.success) {
+                alert('Product Added To Wishlist');
+            }else{
+                alert('Product Already in Wishlist');
+            }
+        }
+        else {
+            alert('Please Log In First');
+        }
+    };
     return (
-        <button className="w-full flex justify-center items-center gap-1 bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded">
+        <button onClick={handleAddToWishlist} className="w-full flex justify-center items-center gap-1 bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded">
             Add to Wishlist
         </button>
 
