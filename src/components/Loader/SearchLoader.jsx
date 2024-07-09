6a6597dfd2bb56d7c07b09f7fb1c17dc8098199d
Bijan <bijan2059@gmail.com>
2024-07-09 17:07:46 +0545

    made a search loader

---
+++ src/components/Loader/SearchLoader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const SearchLoader = ({count}) => {
+    return (
+        <>
+        {Array.from({ length: count || 4 }, (_, index) => (
+            <div role="status" className="animate-pulse p-2 border-b border-gray-200 dark:border-gray-600 flex gap-2">
+            <div className='aspect-square p-8 md:p-10 bg-gray-200'>
+            </div>
+            <div>
+                <p className='bg-gray-200 w-36 h-4 mb-2'></p>
+                <p className='bg-gray-200 w-32 h-4'></p>
+            </div>
+        </div>
+         ))}
+        </>
+
+    )
+}
+
+export default SearchLoader
