17ab6d5bcd5a3ed895302a0236fa71183afbdc76
Bijan <bijan2059@gmail.com>
2024-07-09 14:44:27 +0545

    image validation added

--- src/components/Design/ProfileComponent/ProfileCard.jsx
+++ src/components/Design/ProfileComponent/ProfileCard.jsx
@@ -1,8 +1,9 @@
 const ProfileCard = ({user}) => {
+  const imageUrl = user.image ? `http://localhost:3001/users/${user.image}` : 'guitar.jpg';
   return (
     <div className="w-full md:w-11/12 bg-inherit border  border-gray-200 rounded-lg shadow dark:border-gray-700">
     <div className="flex items-center gap-2 p-3 md:p-5 md:gap-5">
-      <img className="w-28 h-28 md:w-40 md:h-40 mb-3 rounded-full shadow-lg" src="guitar.jpg" alt="Bonnie image" />
+      <img className="w-28 h-28 md:w-40 md:h-40 mb-3 rounded-full shadow-lg" src={imageUrl} alt="profile image" />
       <div className="flex flex-col p-2">
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white lg:text-xl">{user.first_name} {user.last_name}</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400 lg:text-lg">{user.address}</span>
