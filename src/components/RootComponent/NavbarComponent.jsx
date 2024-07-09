744f0f5dbc7b837c64b833061a971da2ac67e933
Bijan <bijan2059@gmail.com>
2024-07-09 17:28:03 +0545

    added link to logo

--- src/components/RootComponent/NavbarComponent.jsx
+++ src/components/RootComponent/NavbarComponent.jsx
@@ -6,10 +6,12 @@ import SearchBarComponent from "./SearchBarComponent";
 const NavbarComponent = () => {
     return (
         <nav className='sticky top-0 flex flex-col z-10 items-center p-5 bg-white dark:bg-gray-900 border-b-2 border-gray-100 dark:border-black md:flex-row md:justify-between md:px-10 md:py-6 md:border-gray-200'>
+            <Link href={'/'}>
             <div className='flex items-center text-2xl font-bold mb-3 md:mb-0 md:text-3xl'>
                 <h1 className="dark:text-white">GuitarShop</h1>
                 <img src="/logo.png" className="w-12 h-12 object-cover md:w-14 md:h-14" />
             </div>
+            </Link>
             <SearchBarComponent />
             <Links/>
         </nav>
