b0689bb00c5f90a7ffdf43c5ec16ff6f5a2edc06
Bijan <bijan2059@gmail.com>
2024-07-09 14:44:13 +0545

    added empty message

--- src/app/(root)/wishlist/page.jsx
+++ src/app/(root)/wishlist/page.jsx
@@ -28,20 +28,29 @@ const Wishlist = () => {
 
   return (
     <>
-    <div className="md:flex md:w-3/4 gap-2 md:mx-auto">
-        <h1 className='text-xl px-5 md:px-0 mt-4 font-bold md:text-2xl mb-2'>WishList</h1>
+<div className="md:flex md:w-3/4 gap-2 md:mx-auto">
+  <h1 className='text-xl px-5 md:px-0 mt-4 font-bold md:text-2xl mb-2'>WishList</h1>
+</div>
+<div className="w-full md-2 md:mb-10 mt-4 px-5 grid grid-cols-2 gap-2 md:px-0 md:w-3/4 md:mx-auto lg:grid-cols-4 md:gap-4">
+  {isLoading ? (
+    // Show loading indicator
+    <ProductCardLoader count={4} />
+  ) : items.length === 0 ? (
+    <div>
+      Your wishlist is empty.
     </div>
-    <div className="w-full md-2 md:mb-10 mt-4 px-5 grid grid-cols-2 gap-2 md:px-0 md:w-3/4 md:mx-auto lg:grid-cols-4 md:gap-4 ">
-        {isLoading ? (
-          // Show loading indicator
-          <ProductCardLoader count={4} />
-        ) : (
-          // Render products
-          items.map((item, index) => (
-            <WishListCard key={item.product_id._id} product={item.product_id} handleRemoveFromWishlist={handleRemoveFromWishlist} />
-          ))
-        )}
-      </div>
+  ) : (
+    // Render products
+    items.map((item) => (
+      <WishListCard
+        key={item.product_id._id}
+        product={item.product_id}
+        handleRemoveFromWishlist={handleRemoveFromWishlist}
+      />
+    ))
+  )}
+</div>
+
 </>
   )
 }
