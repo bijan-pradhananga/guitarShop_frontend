330a4678c1cddba6561e28b18e70a6fa478cfd42
Bijan <bijan2059@gmail.com>
2024-07-28 09:04:08 +0545

    added addProduct and deleteProduct function

--- src/app/(admin)/admin/products/page.jsx
+++ src/app/(admin)/admin/products/page.jsx
@@ -1,53 +1,75 @@
+'use client'
+import ProductPopup from "@/components/Design/PopupComponent/ProductPopup"
+import brand, {fetchBrands} from "@/lib/features/brand"
+import { fetchCategory } from "@/lib/features/category"
+import product, { deleteProduct, fetchProducts } from "@/lib/features/product"
+import { useAppDispatch, useAppSelector } from "@/lib/hooks"
+import { useEffect, useState } from "react"
+
 const page = () => {
+    const dispatch = useAppDispatch()
+    const products = useAppSelector((state) => state.product)
+    const categories = useAppSelector((state)=>state.category)
+    const brands = useAppSelector((state)=>state.brand)
+    const [addPopup,setAddPopup] = useState(false)
+
+    const handleDelete = async (id) =>{
+        let confirm = window.confirm("Are you sure u want to delete this product?");
+        if (confirm) {
+            const result = await dispatch(deleteProduct(id));
+            if (deleteProduct.fulfilled.match(result)) {
+                dispatch(fetchProducts({ url: `product?limit=8` }));
+                alert('Product Deleted Successfully');
+              } else {
+                alert('Unexpected Error Occured');
+              }
+        }
+    }
+
+    useEffect(() => {
+        dispatch(fetchProducts({ url: `product?limit=8` }));
+        dispatch(fetchCategory());
+        dispatch(fetchBrands());
+    }, [])
     return (
         <>
-            <div className='text-2xl font-bold mb-4'>
-                Products
-            </div>
+            <ProductHeader/>
             <div className="bg-gray-50 dark:bg-gray-800 rounded mb-4 p-5 flex gap-4">
-                <div className="flex-1 ">
-                    <form className="w-full mx-auto">
-                        <label
-                            htmlFor="default-search"
-                            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-                        >
-                            Search
-                        </label>
-                        <div className="relative">
-                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg
-                                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                                    />
-                                </svg>
-                            </div>
-                            <input
-                                type="search"
-                                id="default-search"
-                                className="block w-full p-4 ps-10 text-sm text-gray-900 border outline-none border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 dark:focus:border-gray-400"
-                                placeholder="Search Products"
-                                required=""
-                            />
-
-                        </div>
-                    </form>
-
-                </div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white text-md font-semibold md:font-bold p-2 rounded">
+                <ProductSearchBar />
+                <button className="bg-blue-500 hover:bg-blue-700 text-white text-md font-semibold md:font-bold p-2 rounded" 
+                    onClick={()=>{setAddPopup(true)}}
+                >
                     Add Product
                 </button>
             </div>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <ProductsTable products={products} handleDelete={handleDelete}  />
+            </div>
+            {addPopup && <ProductPopup fetchProducts={fetchProducts} dispatch={dispatch} categories={categories} brands={brands} setProductPopup={setAddPopup}/>}
+        </>
+    )
+}
+
+
+const ProductHeader = () => {
+    return (
+        <div className='text-2xl font-bold mb-4'>
+            Products
+        </div>
+    )
+}
+
+
+const ProductsTable = ({ products , handleDelete}) => {
+    return (
+        <>
+            {products.isLoading ? (
+                // Show loading indicator
+                <div>Loading</div>
+            ) : products.data.length === 0 ? (
+                <div>No Products Found</div>
+            ) : (
+                // Render products
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 border dark:border-gray-800">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -55,7 +77,7 @@ const page = () => {
                                 Product name
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                Color
+                                Brand
                             </th>
                             <th scope="col" className="px-6 py-3">
                                 Category
@@ -64,117 +86,92 @@ const page = () => {
                                 Price
                             </th>
                             <th scope="col" className="px-6 py-3">
+                                Quantity
+                            </th>
+                            <th scope="col" className="px-6 py-3">
                                 Action
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        {products.data.map((product, index) => (
+                            <tr
+                                key={index}
+                                className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
                             >
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">Silver</td>
-                            <td className="px-6 py-4">Laptop</td>
-                            <td className="px-6 py-4">$2999</td>
-                            <td className="px-6 py-4 flex gap-2 items-center">
-                                <a
-                                    href="#"
-                                    className="font-medium text-red-500 hover:underline"
+                                <th
+                                    scope="row"
+                                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                                 >
-                                    Delete
-                                </a>
-                                <a
-                                    href="#"
-                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                        </tr>
-                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                Microsoft Surface Pro
-                            </th>
-                            <td className="px-6 py-4">White</td>
-                            <td className="px-6 py-4">Laptop PC</td>
-                            <td className="px-6 py-4">$1999</td>
-                            <td className="px-6 py-4">
-                                <a
-                                    href="#"
-                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                        </tr>
-                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                Magic Mouse 2
-                            </th>
-                            <td className="px-6 py-4">Black</td>
-                            <td className="px-6 py-4">Accessories</td>
-                            <td className="px-6 py-4">$99</td>
-                            <td className="px-6 py-4">
-                                <a
-                                    href="#"
-                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                        </tr>
-                        <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                Google Pixel Phone
-                            </th>
-                            <td className="px-6 py-4">Gray</td>
-                            <td className="px-6 py-4">Phone</td>
-                            <td className="px-6 py-4">$799</td>
-                            <td className="px-6 py-4">
-                                <a
-                                    href="#"
-                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                Apple Watch 5
-                            </th>
-                            <td className="px-6 py-4">Red</td>
-                            <td className="px-6 py-4">Wearables</td>
-                            <td className="px-6 py-4">$999</td>
-                            <td className="px-6 py-4">
-                                <a
-                                    href="#"
-                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                >
-                                    Edit
-                                </a>
-                            </td>
-                        </tr>
+                                    {product.product_name}
+                                </th>
+                                <td className="px-6 py-4">{product.brand_id.brand_name}</td>
+                                <td className="px-6 py-4">{product.category_id.category_name}</td>
+                                <td className="px-6 py-4">{product.price}</td>
+                                <td className="px-6 py-4">{product.quantity}</td>
+                                <td className="px-6 py-4 flex gap-2 items-center">
+                                    <span
+                                        href="#"
+                                        className="font-medium text-red-500 hover:underline cursor-pointer"
+                                        onClick={()=>{handleDelete(product._id)}}
+                                    >
+                                        Delete
+                                    </span>
+                                    <span
+                                        href="#"
+                                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer"
+                                    >
+                                        Edit
+                                    </span>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
-            </div>
+            )}
         </>
+    )
+}
 
+const ProductSearchBar = () => {
+    return (
+        <div className="flex-1 ">
+            <form className="w-full mx-auto">
+                <label
+                    htmlFor="default-search"
+                    className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                    Search
+                </label>
+                <div className="relative">
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg
+                            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                            />
+                        </svg>
+                    </div>
+                    <input
+                        type="search"
+                        id="default-search"
+                        className="block w-full p-4 ps-10 text-sm text-gray-900 border outline-none border-gray-300 rounded-lg focus:ring-1 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 dark:focus:border-gray-400"
+                        placeholder="Search Products"
+                        required=""
+                    />
+
+                </div>
+            </form>
+        </div>
     )
 }
 
