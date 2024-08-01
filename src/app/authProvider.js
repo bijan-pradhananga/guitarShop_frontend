f7b8186b9212a9d66dcbdcd6b2437cb36eedb6d0
Bijan <bijan2059@gmail.com>
2024-08-01 15:18:06 +0545

    made slight changes to authProvider

--- src/app/authProvider.js
+++ src/app/authProvider.js
@@ -8,17 +8,17 @@ const withAuth = (WrappedComponent) => {
     const Wrapper = (props) => {
         const router = useRouter();
         const dispatch = useDispatch();
-        const { data, error } = useSelector((state) => state.user);
+        const { data } = useSelector((state) => state.user);
 
-        useEffect(() => {
-            dispatch(checkAuth());
-        }, [dispatch]);
-
-        useEffect(() => {
-            if (!data && error === 'unauthorized') {
+        const handleAuth = async () => {
+            const res = await dispatch(checkAuth());
+            if (!res.payload.success) {
                 router.push('/login');
             }
-        }, [ data, error, router]);
+        }
+        useEffect(() => {
+            handleAuth()
+        }, [dispatch]);
 
         return data ? <WrappedComponent {...props} /> : null;
     };
