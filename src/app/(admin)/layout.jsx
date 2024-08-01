5f057605ae445d1fcda6d8cab529afe66c399776
Bijan <bijan2059@gmail.com>
2024-08-01 15:18:39 +0545

    applied checkAdminAuth

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -1,23 +1,22 @@
-import SidebarComponent from "@/components/RootComponent/SidebarComponent";
-import { ThemeSwitcher } from "@/components/UI/ThemeSwitch";
+'use client'
+import AdminHeaderComponent from "@/components/Design/AdminComponent/AdminHeaderComponent";
+import SidebarComponent from "@/components/Design/AdminComponent/SidebarComponent";
+import withAdminAuth from "../adminAuthProvider";
 
-export default function RootLayout({
-    children,
-  }) {
+const RootLayoutComponent = ({ children }) => {
     return (
         <>
-            <SidebarComponent/>
+            <SidebarComponent />
             <div className="sm:ml-64 dark:bg-gray-900">
-                <div className="w-full bg-gray-100 dark:bg-gray-900 dark:border-b border-gray-700 py-4 px-8 flex justify-end gap-3">
-                    <div className="text-3xl grid items-center">
-                        <ThemeSwitcher/>
-                    </div>
-                    <div className="rounded-full aspect-square bg-gray-200 w-12"></div>
-                </div>
+                <AdminHeaderComponent />
                 <div className="px-8 py-5">
                     {children}
                 </div>
             </div>
         </>
-    )
-  }
+    );
+};
+
+const RootLayout = withAdminAuth(RootLayoutComponent);
+
+export default RootLayout;
