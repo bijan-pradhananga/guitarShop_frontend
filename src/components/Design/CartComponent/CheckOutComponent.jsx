df86a9daf1f1b5ae88c28028b2c4cdc99e6e0d83
Bijan <bijan2059@gmail.com>
2024-08-06 13:37:14 +0545

    added total

--- src/components/Design/CartComponent/CheckOutComponent.jsx
+++ src/components/Design/CartComponent/CheckOutComponent.jsx
@@ -1,11 +1,11 @@
-const CheckOutComponent = () => {
+const CheckOutComponent = ({total}) => {
   return (
     <>
     <div className="w-full p-3 dark:border-gray-700 dark:border-2 lg:p-5 rounded shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
         <h1 className='text-md font-bold lg:text-xl  mb-4'>Cart Totals</h1>
         <div className="flex justify-between my-2 pb-4 border-b text-sm lg:text-lg border-gray-400 ">
             <h1 className="font-semibold">Total</h1>
-            <h1 >Rs. 20000</h1>
+            <h1 >Rs. {total}</h1>
         </div>
         <button className="w-full bg-blue-500 my-3 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded">
             Checkout
