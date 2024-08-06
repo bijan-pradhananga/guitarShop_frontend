a0833fd74fc80c7d52e905d3440f4cdbef2d8c82
Bijan <bijan2059@gmail.com>
2024-08-06 08:03:59 +0545

    added icon

--- src/components/Design/AdminComponent/SidebarComponent.jsx
+++ src/components/Design/AdminComponent/SidebarComponent.jsx
@@ -50,7 +50,7 @@ const SidebarComponent = () => {
                             href="category"
                             className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                         >
-                            <svg
+                            {/* <svg
                                 className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                                 aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg"
@@ -58,7 +58,15 @@ const SidebarComponent = () => {
                                 viewBox="0 0 18 20"
                             >
                                 <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                            </svg> */}
+                            <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                                aria-hidden="true" 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill="currentColor"
+                                 viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M4.857 3A1.857 1.857 0 0 0 3 4.857v4.286C3 10.169 3.831 11 4.857 11h4.286A1.857 1.857 0 0 0 11 9.143V4.857A1.857 1.857 0 0 0 9.143 3H4.857Zm10 0A1.857 1.857 0 0 0 13 4.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 9.143V4.857A1.857 1.857 0 0 0 19.143 3h-4.286Zm-10 10A1.857 1.857 0 0 0 3 14.857v4.286C3 20.169 3.831 21 4.857 21h4.286A1.857 1.857 0 0 0 11 19.143v-4.286A1.857 1.857 0 0 0 9.143 13H4.857Zm10 0A1.857 1.857 0 0 0 13 14.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 21 19.143v-4.286A1.857 1.857 0 0 0 19.143 13h-4.286Z" clip-rule="evenodd" />
                             </svg>
+
                             <span className="flex-1 ms-3 whitespace-nowrap">Category</span>
                         </Link>
                     </li>
@@ -81,7 +89,7 @@ const SidebarComponent = () => {
                         </Link>
                     </li>
                     <li>
-                        <AdminLogoutComponent/>
+                        <AdminLogoutComponent />
                     </li>
                 </ul>
             </div>
