07914631199a417bd8a8bea06d5bf5ff102172a6
Bijan <bijan2059@gmail.com>
2024-08-12 13:53:10 +0545

    made buy now button

---
+++ src/components/hero/BuyNowBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BuyNowBtn = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold p-2 rounded"
+    // onClick={handleAddToCart}
+    >
+        Buy Now
+    </button>
+  )
+}
+
+export default BuyNowBtn
