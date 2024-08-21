b807c0c5a5436c7bbb3d7e03f66d4826ca38f696
Bijan <bijan2059@gmail.com>
2024-08-21 14:26:13 +0545

    added async function to buynow btn

--- src/components/hero/BuyNowBtn.jsx
+++ src/components/hero/BuyNowBtn.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client'
+import { buyNow } from "@/lib/features/order";
+import { checkAuth } from "@/lib/features/user";
+import { useAppDispatch } from "@/lib/hooks";
 
-const BuyNowBtn = () => {
+const BuyNowBtn = ({product,fetchSingleProduct,quantity}) => {
+  const dispatch = useAppDispatch();
+
+    const handleBuyNow = async () => {
+      const confirm = window.confirm('Are you sure to place the order?')
+      if (confirm) {
+        const res = await dispatch(checkAuth())
+        if (res.payload.success) {
+            const user_id = res.payload.user._id 
+            await dispatch(buyNow({ product_id: product._id, user_id:user_id, quantity }));
+            alert('Order Placed successfully');
+            dispatch(fetchSingleProduct(product._id));
+        }
+        else {
+            alert('Please Log In First');
+        }
+      }
+
+  };
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold p-2 rounded"
-    // onClick={handleAddToCart}
+    onClick={handleBuyNow}
     >
         Buy Now
     </button>
