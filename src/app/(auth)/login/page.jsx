246ca63f7f56469d92950b973c4f379315c4558f
Bijan <bijan2059@gmail.com>
2024-08-30 12:47:50 +0545

    removed remember me

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -66,33 +66,7 @@ const LoginForm = () => {
                                     required
                                 />
                             </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input
-                                            id="remember"
-                                            aria-describedby="remember"
-                                            type="checkbox"
-                                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                            required=""
-                                        />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label
-                                            htmlFor="remember"
-                                            className="text-gray-500 dark:text-gray-300"
-                                        >
-                                            Remember me
-                                        </label>
-                                    </div>
-                                </div>
-                                <a
-                                    href="#"
-                                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                                >
-                                    Forgot password?
-                                </a>
-                            </div>
+
                             <button
                                 type="submit"
                                 className="w-full text-white bg-blue-500 hover:bg-blue-700  font-medium rounded-lg text-sm px-5 py-2.5 text-center "
