4f807ab1cfbfe03f7cfb0613ef438cb32a3f1f42
Bijan <bijan2059@gmail.com>
2024-09-16 19:42:16 +0545

    separated components

--- src/components/Design/CartComponent/CheckOutComponent.jsx
+++ src/components/Design/CartComponent/CheckOutComponent.jsx
@@ -20,8 +20,20 @@ const CheckOutComponent = ({ items, total, userId, fetchCartItems, dispatch }) =
   return (
     <>
       <div className="w-full p-3 dark:border-gray-700 dark:border-2 lg:p-5 rounded shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-        <h1 className='text-md font-bold lg:text-xl  mb-4'>Order Summary</h1>
-        <div className="border p-2 border-solid border-blueGray-200 dark:border-gray-700 rounded-b mb-4">
+        <OrderSummary items={items} total={total}/>
+        <CheckOutFooter handleCheckout={handleCheckout}/>
+      </div>
+    </>
+  )
+}
+
+export default CheckOutComponent
+
+const OrderSummary = ({items,total}) =>{
+  return (
+      <>
+              <h1 className='text-md font-bold lg:text-xl  mb-4'>Order Summary</h1>
+        <div className="font-semibold border p-2 border-solid border-blueGray-200 dark:border-gray-700 rounded-b mb-4 text-sm lg:text-md">
           {
             items.map((item, index) => (
               <>
@@ -33,19 +45,22 @@ const CheckOutComponent = ({ items, total, userId, fetchCartItems, dispatch }) =
             ))
           }
         </div>
-
         <div className="flex justify-between mx-2 py-2  border-b text-sm lg:text-lg border-gray-400 ">
           <h1 className="font-semibold">Total</h1>
           <h1 >Rs. {total}</h1>
         </div>
-        <button className="w-full bg-blue-500 my-3 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded"
-          onClick={handleCheckout}
-        >
-          Checkout
-        </button>
-      </div>
-    </>
+      </>
   )
 }
 
-export default CheckOutComponent
+const CheckOutFooter = ({handleCheckout}) =>{
+  return (
+    <div className="px-1 ">
+    <button className="w-full bg-blue-500 my-3 hover:bg-blue-700 text-white font-semibold md:font-bold py-2 rounded"
+      onClick={handleCheckout}
+    >
+      Checkout
+    </button>
+    </div>
+  )
+}
