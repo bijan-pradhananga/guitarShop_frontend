c395297e9d6e4807f5cad7f208056c091d77ce68
Bijan <bijan2059@gmail.com>
2024-09-16 18:37:23 +0545

    changed buy now btn function

--- src/components/hero/BuyNowBtn.jsx
+++ src/components/hero/BuyNowBtn.jsx
@@ -1,32 +1,12 @@
 'use client'
-import { buyNow } from "@/lib/features/order";
-import { checkAuth } from "@/lib/features/user";
-import { useAppDispatch } from "@/lib/hooks";
 
-const BuyNowBtn = ({product,fetchSingleProduct,quantity}) => {
-  const dispatch = useAppDispatch();
+const BuyNowBtn = ({ setPopup }) => {
 
-    const handleBuyNow = async () => {
-      const confirm = window.confirm('Are you sure to place the order?')
-      if (confirm) {
-        const res = await dispatch(checkAuth())
-        if (res.payload.success) {
-            const user_id = res.payload.user._id 
-            await dispatch(buyNow({ product_id: product._id, user_id:user_id, quantity }));
-            alert('Order Placed successfully');
-            dispatch(fetchSingleProduct(product._id));
-        }
-        else {
-            alert('Please Log In First');
-        }
-      }
-
-  };
   return (
     <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold md:font-bold p-2 rounded"
-    onClick={handleBuyNow}
+      onClick={() => setPopup(true)}
     >
-        Buy Now
+      Buy Now
     </button>
   )
 }
