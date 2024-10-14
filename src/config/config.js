2bdd0036f6c99176e6e51a303b6a90f7a8241d82
Bijan Pradhananga <100182448+bijan-pradhananga@users.noreply.github.com>
2024-10-14 09:10:50 +0545

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "guitarshopbackend-production.up.railway.app",
   withCredentials: true
 }
 );
